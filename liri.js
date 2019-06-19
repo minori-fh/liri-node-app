981a1e7ffaab3aed011b4d0e65e6fa0fba92b02d
Minori Hashimoto <minori.fh@gmail.com>
2019-06-19 11:41:38 -0700

    put a conditional statement so that if user leaves movie name input blank, console.log a movie recommendation (Mr. Nobody)

--- liri.js
+++ liri.js
@@ -28,7 +28,12 @@ function moviePrompt(){
         },
     ])
     .then(function(inquirerResponse){
-        movie = inquirerResponse.movie
+        if (inquirerResponse.movie === ""){
+            console.log("You didn't input anything! Here's a movie rec: ")
+            movie = "Mr. Nobody"
+        } else {
+            movie = inquirerResponse.movie
+        }
 
         axios.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy").then(
             function(response){
@@ -39,89 +44,91 @@ function moviePrompt(){
                 console.log("Country produced : " + response.data.Country)
                 console.log("Language : " + response.data.Language)
                 console.log("Plot : " + response.data.Plot)
-                console.log("Actors : " + response.data.Actors)
-            }
-            // .catch(function(error){
-            //     if (error) {
-            //         return console.log(error);
-            //       }
-            // })
-        )
+                console.log("Actor(s) : " + response.data.Actors)
+            })
+            .catch(function(error){
+                if (error) {
+                    return console.log(error, error.message);
+                  }
+            })
+        
     });
 }
 
 function concertPrompt(){
     inquirer
-        .prompt([
-            {
-            type: "input",
-            message: "Which artist would you like to get concert information about?",
-            name: "concert"
-            },
-        ])
-        .then(function(inquirerResponse){
-            artist = inquirerResponse.concert
-            console.log("picked artist" + artist)
+    .prompt([
+        {
+        type: "input",
+        message: "Which artist would you like to get concert information about?",
+        name: "concert"
+        },
+    ])
+    .then(function(inquirerResponse){
+        artist = inquirerResponse.concert
+        console.log("picked artist" + artist)
 
-            axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp&date=upcoming").then(
-                function(response){
-                    console.log("Venue name: " + response.data[0].venue.name)
-                    console.log("Venue location: " + response.data[0].venue.city + ", " + response.data[0].venue.region + " " + response.data[0].venue.country)
-                    
-                    // format date with moment
-                    date = response.data[0].datetime.slice(0,10)
-                    formattedDate = moment(date, "YYYY-MM-DD").format("MM/DD/YYYY")
-                    console.log("Event date: " + formattedDate)
-                }
-            )
-        })
+        axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp&date=upcoming").then(
+            function(response){
+                console.log("Venue name: " + response.data[0].venue.name)
+                console.log("Venue location: " + response.data[0].venue.city + ", " + response.data[0].venue.region + " " + response.data[0].venue.country)
+                
+                // format date with moment
+                date = response.data[0].datetime.slice(0,10)
+                formattedDate = moment(date, "YYYY-MM-DD").format("MM/DD/YYYY")
+                console.log("Event date: " + formattedDate)
+            }
+        )
+    })
 }
 
 function spotifyPrompt(){
     inquirer
-        .prompt([
-            {
-            type: "input",
-            message: "Which song would you like to get information about?",
-            name: "song"
-            },
-        ])
-        .then(function(inquirerResponse){
-            song = inquirerResponse.song
-            console.log("You picked a song!" + song)
+    .prompt([
+        {
+        type: "input",
+        message: "Which song would you like to get information about?",
+        name: "song"
+        },
+    ])
+    .then(function(inquirerResponse){
+        song = inquirerResponse.song
+        console.log("You picked a song!" + song)
 
-            spotify.search({ type: 'track', query: song},
-                function(err, data){
+        spotify.search({ type: 'track', query: song},
+            function(err, data){
 
-                    if (err){
-                        return console.log("Error occured: " + err)
-                    }
-            
-                    console.log("Artist(s): " + data.tracks.items[0].album.artists[0].name)
-
-                    if (data.tracks.items[0].album.album_type === "album"){ //freaky is a single
-                        console.log("Song name: " + song)
-                    } else {
-                        console.log("Song name: " + data.tracks.items[0].album.name)
-                    }
-
-                    console.log("Song preview: " + data.tracks.items[0].album.external_urls.spotify)
-
-                    if (data.tracks.items[0].album.album_type === "single"){ //freaky is a single
-                        console.log("Album name: N/A - this is a single!")
-                    } else if (data.tracks.items[0].album.album_type === "album"){
-                        console.log("Album name: " + data.tracks.items[0].album.name)
-                    }
+                if (err){
+                    return console.log("Error occured: " + err)
                 }
-                )
-        })
+        
+                console.log("Artist(s): " + data.tracks.items[0].album.artists[0].name)
+
+                //Check if the song is part of an album
+                if (data.tracks.items[0].album.album_type === "album"){ //ex: freaky is a single
+                    console.log("Song name: " + song)
+                } else {
+                    console.log("Song name: " + data.tracks.items[0].album.name)
+                }
+                
+                console.log("Song preview: " + data.tracks.items[0].album.external_urls.spotify)
+
+                //Check if the song is a single
+                if (data.tracks.items[0].album.album_type === "single"){ //ex: formation is from an album
+                    console.log("Album name: N/A - this is a single!")
+                } else if (data.tracks.items[0].album.album_type === "album"){
+                    console.log("Album name: " + data.tracks.items[0].album.name)
+                }
+            }
+            )
+    })
 }
 
 
-// Create a "Prompt" with a series of questions.
+// Create a "Prompt" with question
 inquirer
   .prompt([
-    // Here we give the user a list to choose from.
+    // Give the user a list to choose from
     {
       type: "list",
       message: "Which command would you like to run?",
