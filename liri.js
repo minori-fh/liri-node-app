8129c180125f68ec9dd0ca49aa7e18b24509b008
Minori Hashimoto <minori.fh@gmail.com>
2019-06-18 19:30:44 -0700

    add function to inquire user input on artist

--- liri.js
+++ liri.js
@@ -1,5 +1,57 @@
-require("dotenv").config();
+var dotenv = require("dotenv").config();
+var axios = require("axios");
+var moments = require("moment")
+var inquirer = require("inquirer");
 
 var keys = require("./keys.js");
+// var spotify = new Spotify(keys.spotify);
 
-var spotify = new Spotify(keys.spotify);
+//Function declarations
+function moviePrompt(){
+    inquirer
+        .prompt([
+            {
+            type: "input",
+            message: "What movie would you like to get information about?",
+            name: "movie"
+            },
+        ])
+}
+
+function concertPrompt(){
+    inquirer
+        .prompt([
+            {
+            type: "input",
+            message: "Which artist would you like to get concert information about?",
+            name: "concert"
+            },
+        ])
+}
+
+
+// Create a "Prompt" with a series of questions.
+inquirer
+  .prompt([
+    // Here we give the user a list to choose from.
+    {
+      type: "list",
+      message: "Which command would you like to run?",
+      choices: ["concert-this", "spotify-this-song", "movie-this", "do-what-it-says"],
+      name: "command"
+    },
+  ])
+  .then(function(inquirerResponse){
+      if (inquirerResponse.command === "concert-this"){
+          concertPrompt();
+
+      } else if (inquirerResponse.command === "spotify-this-song") {
+
+      } else if (inquirerResponse.command === "movie-this") {
+          moviePrompt();
+
+      } else if (inquirerResponse.command === "do-what-it-says"){
+
+      }
+
+  });
